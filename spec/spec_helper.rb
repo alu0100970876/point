aa5a19686694f8857cd3c07953b5dca6947c9e25
Miguel Jiménez Gomis <alu0100970876@ull.edu.es>
2017-11-28 09:55:30 +0000

    Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@ require "coveralls"
 require "bundler/setup"
 require "point"
 
+Coveralls.wear!
+
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
